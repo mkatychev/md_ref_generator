47c86a5995b8dcfba55d3df1b009649f5a199c41
Mikhail Katychev <mkatych@gmail.com>
2018-10-15 14:03:50 -0500

    RC 1 with readme

---
+++ README.md
@@ -0,0 +1,67 @@
+# md-ref-generator
+
+This is a python3 command line utility to automate validating and generating section references for markdown documents.
+
+* A section in markdown is considered any line that starts with 1 or more `#` followed by a space.
+
+* Sections should be surrounded by mirrored curly double citation marks like so: `“Section Reference”` to be considered a candidate for evaluation. 
+
+* The process is currently hardcoded to look for anything inside double citation marks and exclude any cited text that is already surrounded by square brackets, ex: `...[“Section Reference”]...` will be ignored.
+
+* Anchor link generation tries to mirror [md-book](https://github.com/rust-lang-nursery/mdBook), see assertion tests for more details.
+
+```
+usage: generate_refs.py [-h] [-f] [-d] [-q] [-i] [-r]
+                        [md_directory [md_directory ...]]
+
+Create and validate rust-book mentions of other sections
+
+positional arguments:
+  md_directory          desired input filepath, can be any number of filepaths
+                        and valid files
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f, --flag-dead-links
+                        flag links that potentially reference sections not
+                        present in the book
+  -d, --dry-run         print out references that will be replaced in the md
+                        document
+  -q, --quiet           do not print optional args to console
+  -i, --ignore-md       ignore file endings when passing individual files
+  -r, --references      display mapped dictionary of genereated references
+
+```
+
+## Examples
+renerate references to and from 
+
+```
+python3 generate_refs.py src/ch18-01-all-the-places-for-patterns.md src/ch18-03-pattern-syntax.md --dry-run
+```
+
+produces an output where only references in both files are considered:
+
+```
+	ch18-01-all-the-places-for-patterns=#=#=#=#=#=#=#=#=#=#=#=#
+	“Ignoring Values in a Pattern”
+	 | | | | | | | | | | | | | | | | | | | |
+	 v v v v v v v v v v v v v v v v v v v v
+	[“Ignoring Values in a Pattern”][ignoring-values-in-a-pattern]
+	
+	
+	“Ignoring Values in a Pattern”
+	 | | | | | | | | | | | | | | | | | | | |
+	 v v v v v v v v v v v v v v v v v v v v
+	[“Ignoring Values in a Pattern”][ignoring-values-in-a-pattern]
+	
+	
+	ch18-03-pattern-syntax=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+	“Extra
+	Conditionals with Match Guards”
+	 | | | | | | | | | | | | | | | | | | | |
+	 v v v v v v v v v v v v v v v v v v v v
+	[“Extra
+	Conditionals with Match Guards”](#extra-conditionals-with-match-guards)
+	
+```
--- generate_refs.py
+++ generate_refs.py
@@ -3,71 +3,56 @@ import re
 import os
 import argparse
 from pprint import pprint
-from collections import defaultdict
-
+from collections import defaultdict, OrderedDict
 
 # This python script is meant to autogenerate markdown links
 # and validate section references for the markdown rust-book
+
+
 def format_anchor(line):
-   return re.sub('[^\w-]+', '', '-'.join(line.split())).lower()
+    return re.sub('[^\w-]+', '', '-'.join(line.split())).lower()
 
 
-# format_achor unit test
-assert format_anchor(
-    'Preventing Reference Cycles: Turning an Rc<T> into a Weak<T>') == (
-    'preventing-reference-cycles-turning-an-rct-into-a-weakt')
-# it is necessary to strip nonalphas after the hypens to mirror present mdbook
-# functionality when a header string ends with a spaced separated nonalpha
-assert format_anchor(
-    'Specify multiple traits with +') == (
-    'specify-multiple-traits-with-')
+def normalize_header(header):
+    return ''.join(i for i in header if i.isspace() or i.isalpha())
 
 
-def create_entry(line, fname):
-    return { line: {
+def create_entry(header, fname):
+    return { normalize_header(header): {
         'filename': fname,
-        'anchor-id': format_anchor(line)}
-        }
+        'anchor-id': format_anchor(header)}
+            }
 
 
-# create_entry unit test
-create_e_str = 'Using `Result<T, E>` in tests'
-create_e_fname = 'ch11-01-writing-tests'
-create_entry_out = {
-    'Using `Result<T, E>` in tests': {
-        'filename': 'ch11-01-writing-tests',
-        'anchor-id': 'using-resultt-e-in-tests'
-        }
-    }
-assert create_entry(create_e_str, create_e_fname) == create_entry_out
-
-
-re_header = re.compile('^#{1,3} .+\n?$')
-def gather_reference(fname, page, ref_list, header_pattern):
+def gather_reference(fname, page, ref_list, header_pattern, common_headers=['Summary']):
     inside_codeblock = False
     for line in page:
-        # determine if this is a chapter start heading
-        # this is to avoid scraping codeblock comments
+        # strip indention
+        line = line.lstrip('> ')
+        # avoid scraping codeblock comments
         if line.startswith('```'):
             # turn on/off gathering
             inside_codeblock = inside_codeblock ^ True
+
         if inside_codeblock is False and re.match(re_header, line):
-            yield create_entry(line.lstrip('# ').rstrip('\n'), fname)
+            formatted_title = line.lstrip('# ').rstrip('\n')
+            if formatted_title not in common_headers:
+                yield create_entry(formatted_title, fname)
     return
 
 
-re_section = re.compile(r'(?<!\[)?“.+\n?.+\n?”(?!\])', re.MULTILINE)
 def insert_reference(fname, ref_list, page, section_pattern, **kwargs):
     modified = False
+    replaced = set()
     link_bank = set()
     dead_links = []
     dry_runs = []
     for quoted_match in re.finditer(section_pattern, page):
         # strip any whitespace to be replaced with a newline
         quoted_string = quoted_match.group()
-        match_key = quoted_match.group().strip('“”').replace('\n', ' ')
+        match_key = normalize_header(quoted_match.group().strip('“”').replace('\n', ' '))
         # ignore section reference if starts with lowercase
-        if match_key[0].islower():
+        if match_key[0].islower() or quoted_string in replaced:
             continue
         if match_key in ref_list and quoted_string in page:
             # create '#standalone_id' link if reference is on the same page
@@ -77,43 +62,77 @@ def insert_reference(fname, ref_list, page, section_pattern, **kwargs):
                 replace_as = f'[{quoted_string}]({section_link})'
             else:
                 external_section_link = (
-                        f'{ref_list[match_key]["filename"]}.html#{section_id}')
+                    f'{ref_list[match_key]["filename"]}.html#{section_id}')
                 replace_as = f'[{quoted_string}][{section_id}]'
+                # append external links to bottom page as last step
                 link_bank.add(f'[{section_id}]: {external_section_link}')
             # handle dry run optional arg
             if kwargs.get('dry_run'):
                 dry_runs.append((quoted_string, replace_as))
             else:
-                page = page.replace(quoted_string, replace_as, 1)
+                page = page.replace(quoted_string, replace_as)
+                replaced.add(quoted_string)
                 modified = True
         # handle optional flagging of dead links
         elif kwargs.get('flag_dead_links'):
             dead_links.append(quoted_string)
-    if dry_runs and kwargs.get('quiet') is False:
-        filler = (60 - len(fname))//2
-        print(fname + '=#'*filler)
-        for replacement in dry_runs:
-            print(replacement[0])
-            print(' |'*20)
-            print(' v'*20)
-            print(replacement[1])
-            print('\n')
-    if dead_links and kwargs.get('quiet') is False:
-        print(fname + ' dead links:')
-        for link in dead_links:
-            print(f'[{link}] is possibly a dead link')
+    if kwargs.get('quiet') is False:
+        if dry_runs:
+            filler = (60 - len(fname))//2
+            print(fname + '=#'*filler)
+            for replacement in dry_runs:
+                print(replacement[0])
+                print(' |'*20)
+                print(' v'*20)
+                print(replacement[1])
+                print('\n')
+        if dead_links:
+            print(fname + ' dead links:')
+            for link in dead_links:
+                print(f'[{link}] is possibly a dead link')
     if modified:
-        return page + '\n' + '\n'.join(link_bank)
+        return page + '\n' + '\n'.join(sorted(link_bank))
+
+# regex patterns
+
+# identify headers starting with one to three hashes
+re_header = re.compile('^#{1,4} .+\n?$')
+# identify passages surrounded by culry quotes
+re_section = re.compile(r'(?<!\[)?“.+\n?.+\n?”(?!\])', re.MULTILINE)
+# # # # # # # # # # # # # # #   
+# Assertion Tests
+# # # # # # # # # # # # # # #   
+
+# format_achor unit test
+assert format_anchor(
+    'Preventing Reference Cycles: Turning an Rc<T> into a Weak<T>') == (
+        'preventing-reference-cycles-turning-an-rct-into-a-weakt')
+
+# it is necessary to strip nonalphas after the hypens to mirror present mdbook
+# functionality when a header string ends with a spaced separated nonalpha
+assert format_anchor(
+    'Specify multiple traits with +') == (
+        'specify-multiple-traits-with-')
+
+# create_entry unit test
+create_e_str = 'Using `Result<T, E>` in tests'
+create_e_fname = 'ch11-01-writing-tests'
+create_entry_out = {
+    'Using ResultT E in tests': {
+        'filename': 'ch11-01-writing-tests',
+        'anchor-id': 'using-resultt-e-in-tests'
+    }
+}
+assert create_entry(create_e_str, create_e_fname) == create_entry_out
 
 # insert_reference unit test
 test_insert = (
     'ch11-01-writing-test',
-    {'Concatenation with the `+` Operator or the `format!` Macro': {
+    {'Concatenation with the  Operator or the format Macro': {
         'filename': 'ch08-02-strings',
         'anchor-id': 'concatenation-with-the--operator-or-the-format-macro'
-        }
-    }, (
-"""You can also add a custom message to be printed with the failure message as
+    }
+     }, ( """You can also add a custom message to be printed with the failure message as
 optional arguments to the `assert!`, `assert_eq!`, and `assert_ne!` macros. Any
 arguments specified after the one required argument to `assert!` or the two
 required arguments to `assert_eq!` and `assert_ne!` are passed along to the
@@ -125,7 +144,7 @@ you’ll have a better idea of what the problem is with the code.[“Concatenati
 with the `+` Operator or the `format!` Macro”]""")
 )
 insert_out = (
-"""You can also add a custom message to be printed with the failure message as
+    """You can also add a custom message to be printed with the failure message as
 optional arguments to the `assert!`, `assert_eq!`, and `assert_ne!` macros. Any
 arguments specified after the one required argument to `assert!` or the two
 required arguments to `assert_eq!` and `assert_ne!` are passed along to the
@@ -136,23 +155,50 @@ messages are useful to document what an assertion means; when a test fails,
 you’ll have a better idea of what the problem is with the code.[“Concatenation
 with the `+` Operator or the `format!` Macro”]
 [concatenation-with-the--operator-or-the-format-macro]: ch08-02-strings.html#concatenation-with-the--operator-or-the-format-macro""")
+
+def line_differ(func_in, expected_out):
+    for func, out in zip(func_in.splitlines(), expected_out.splitlines()):
+        if func != out:
+            print('function out:')
+            print(func)
+            print('expected output:')
+            print(out)
+
 assert insert_reference(*(test_insert), re_section) == insert_out
 
 
-def main(src_path, **kwargs):
+def main(src_input, **kwargs):
     # generate array of tuples with each markdown absolute filepath
     # and filename stripped of extension
-    src =  [(file.rstrip('.md'), os.path.join(src_path, file))
-            for file in os.listdir(src_path) if file.endswith('.md')]
-    heading_list = defaultdict(dict)
+    src = []
+
+    def generate_filepaths(in_paths):
+        return ((file.rstrip('.md'), os.path.join(in_paths, file))
+                for file in os.listdir(in_paths) if file.endswith('.md'))
+
+    for path in src_input:
+        if os.path.isdir(path):
+            src.extend(list(generate_filepaths(path)))
+        elif os.path.isfile(path):
+            split_p = os.path.splitext(path)
+            if split_p[1] != '.md' and kwargs.get('ignore_md'):
+                raise Exception(path + 'is not indicated as a markdown file')
+            src.append((os.path.basename(split_p[0]), path))
+        else:
+            raise FileNotFoundError(path)
+
+
+    # src.sort()
+    heading_list = defaultdict(OrderedDict)
     for fname, doc in src:
         with open(doc, 'r') as file:
             pagelines = file.readlines()
         for ref in gather_reference(fname, pagelines, heading_list, re_header):
             heading_list.update(ref)
 
-    if kwargs.get('print_references'):
+    if kwargs.get('references') and kwargs.get('quiet') is False:
         pprint(heading_list)
+
     for fname, doc in src:
         with open(doc, 'r') as file:
             str_page = file.read()
@@ -163,18 +209,24 @@ def main(src_path, **kwargs):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=(
-            'Create and validate rust-book mentions of other sections'))
-    parser.add_argument('md_directory', type=str,
-            help='desired input filepath')
-    parser.add_argument(
-        '--flag-dead-links', action='store_true',
+    parser = argparse.ArgumentParser( description=(
+        'Create and validate rust-book mentions of other sections'))
+    parser.add_argument('md_directory', nargs='*', type=str,
+        help='desired input filepath, can be any number of filepaths and valid files')
+    parser.add_argument('-f', '--flag-dead-links', action='store_true',
         help='flag links that potentially reference sections not present in the book')
-    parser.add_argument(
-        '--dry-run', action='store_true',
-         help='print out references that will be replaced in the md document')
-    parser.add_argument(
-        '--quiet', action='store_true',
-         help='do not print optional args to console')
+    parser.add_argument('-d', '--dry-run', action='store_true',
+        help='print out references that will be replaced in the md document')
+    parser.add_argument('-q', '--quiet', action='store_true',
+        help='do not print optional args to console')
+    parser.add_argument('-i', '--ignore-md', action='store_true',
+        help='ignore file endings when passing individual files')
+    parser.add_argument( '-r', '--references', action='store_true',
+        help='display mapped dictionary of genereated references')
     args = parser.parse_args()
-    main(args.md_directory, flag_dead_links=args.flag_dead_links, dry_run=args.dry_run, quiet=args.quiet)
+    main(args.md_directory,
+         flag_dead_links=args.flag_dead_links,
+         dry_run=args.dry_run,
+         quiet=args.quiet,
+         ignore_md=args.ignore_md,
+         references=args.references)
